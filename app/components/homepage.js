b15ffceacaeb8093e4aa26c31bfc8dac2ea4afc7
DivyaChinthala <divyachinthala519@gmail.com>
2024-05-30 16:17:28 +0530

    feat: move navbar to seperate component

--- app/components/homepage.js
+++ app/components/homepage.js
@@ -1,13 +1,12 @@
 import { getAuthSession } from "../api/auth/[...nextauth]/route";
-import Navigation from "./navigation";
 import MainSection from "./mainSection";
 import axios from "axios";
-import Logo from "../components/logo";
 import { get } from "lodash";
 import SkillsSection from "./skills";
 import AboutSection from "./aboutSection";
 import Projects from "./projects";
 import Contact from "./contact";
+import Navbar from "./navbar";
 
 export default async function Homepage() {
   const session = await getAuthSession();
@@ -18,32 +17,7 @@ export default async function Homepage() {
   const data = response?.data;
   return (
     <div className="h-screen">
-      <div className={`navbar pl-8 pr-6 py-4 fixed top-0 left-0 right-0  z-50`}>
-        {" "}
-        <div className="flex-1">
-          <a className="font-medium no-underline text-black text-4xl">
-            <span style={{ color: primaryColor, fontWeight: 700 }}></span>
-            <Logo profile={get(data, "profile")} />
-          </a>
-        </div>
-        <div className="flex-none gap-8">
-          <Navigation />
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src={get(data, "profile.profilePicture", "")}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Navbar primaryColor={primaryColor} data={data} />
       <MainSection topSection={get(data, "topSection")} />
       <SkillsSection skills={get(data, "skills")} />
       <AboutSection about={get(data, "about")} />
---
+++ app/components/navbar.js
@@ -0,0 +1,35 @@
+"use client";
+import Navigation from "./navigation";
+import Logo from "../components/logo";
+import { get } from "lodash";
+
+export default function Navbar({ primaryColor, data }) {
+  return (
+    <div className={`navbar pl-8 pr-6 py-4 fixed top-0 left-0 right-0`}>
+      {" "}
+      <div className="flex-1">
+        <a className="font-medium no-underline text-black text-4xl">
+          <span style={{ color: primaryColor, fontWeight: 700 }}></span>
+          <Logo profile={get(data, "profile")} />
+        </a>
+      </div>
+      <div className="flex-none gap-8">
+        <Navigation />
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src={get(data, "profile.profilePicture", "")}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

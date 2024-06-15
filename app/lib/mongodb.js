279a98e5912e823bef5fb8468183aa5be0006e7b
DivyaChinthala <divyachinthala519@gmail.com>
2024-06-15 14:06:44 +0530

    fix: mongodb connection

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,24 +1,33 @@
-// mongodb.js
-
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI + "?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("Add Mongo URI to .env.local");
+}
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  retryWrites: true, // Ensure retryWrites is enabled
+  tls: true, // Ensure TLS is enabled for secure connections
+  tlsAllowInvalidCertificates: false, // Adjust based on your certificate setup
+};
 
 let client;
 let clientPromise;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error("Add Mongo URI to .env.local");
-}
-
 if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so we can reuse the client
+  // across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
+    client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri);
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
